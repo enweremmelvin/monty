1f9cf1d3d9d5273cb5af27c3f0469cb284685656
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-24 00:25:10 +0100

    solution for adv task 7

--- pstr_top_element.c
+++ pstr_top_element.c
@@ -13,29 +13,20 @@ void pstr_top_elements(stack_t **stack, unsigned int line_number)
 	(void) stack;
 	(void) line_number;
 
-	if (temp == NULL)
-		printf("\n");
-
 	while (temp != NULL)
 	{
 		if ((temp->n >= 0) && (temp->n <= 127))
 		{
 			if (temp->n == 0)
-			{
-				printf("\n");
 				break;
-			}
 
 			printf("%c", temp->n);
 		}
 		else
-		{
-			printf("\n");
 			break;
-		}
 
-		if (temp->prev == NULL)
-			printf("\n");
 		temp = temp->prev;
 	}
+
+	printf("\n");
 }
