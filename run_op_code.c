c69fcd724b7551da5ceb9e7501b4dbd2e088e30a
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-23 20:07:30 +0100

    correction for task 0

--- run_op_code.c
+++ run_op_code.c
@@ -12,21 +12,31 @@ stack_t *stack;
 
 int run_op_code(int line_number)
 {
-	int i;
+	int i, check_run_success;
 	instruction_t opcode_list[] = {
 		{"push", push_to_stack},
 		{"pall", print_from_stack},
 		{NULL, NULL}
 	};
 
-	i = 0;
+	i = check_run_success = 0;
 
 	while (opcode_list[i].opcode != NULL)
 	{
 		if (strcmp(command, opcode_list[i].opcode) == 0)
+		{
 			opcode_list[i].f(&stack, line_number);
+			check_run_success += 1;
+		}
 
 		i++;
+	}
+
+	if (check_run_success == 0)
+	{
+		dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n",
+			line_number, command);
+		exit(EXIT_FAILURE);
 	}
 
 	return (0);
