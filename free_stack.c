7852b67be35bd44659e49a3aa0d32bfdb57eae44
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-23 20:15:01 +0100

    correction for task 0

---
+++ free_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * free_stack - free the stack
+ */
+
+void free_stack(void)
+{
+	stack_t *temp_;
+
+	while (*stack != NULL)
+	{
+		temp_ = stack;
+		*stack = (*stack)->next;
+
+		free(temp);
+	}
+}
--- main.c
+++ main.c
@@ -19,5 +19,8 @@ int main(int argc, char *argv[])
 
 	parse_file(argv[1]);
 
+	if (*stack)
+		free_stack();
+
 	return (0);
 }
